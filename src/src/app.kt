efdf1459b770ccfdc2a801b10bebad61e8018492
sverdecadilla <sverdecadilla@danielcastelao.org>
2019-11-29 14:11:28 +0100

    Inicio con app.kt

---
+++ src/src/app.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var miOrdernador = Computer("i5", Mouse(), screen = Screen())
+
+    println("${miOrdernador.cpu} tiene un raton ${miOrdernador.mouse.tipo} y una pantalla ${miOrdernador.screen.marca} " +
+            "una velocidad de ${miOrdernador.cpu}")
+
+}
+
+//LA VIDA ES UNA TOMBOLA TOMBOLA TOM TOM TOMBOLA DE LUG Y DE COLOOOOOOOOR
